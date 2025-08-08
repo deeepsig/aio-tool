291142cf81fb8ea59d794b540839e83cdac38a31
deeepsig <deepakgdkg1g8868@gmail.com>
2025-08-08 16:37:06 +0530

    responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
     <div className="min-h-screen font-sans flex flex-col">
       <div className="flex-1 flex flex-col items-center py-[8vh]">
         <About />
-        <div className="mt-8 flex items-start justify-center">
+        <div className="mt-4 sm:mt-8 flex items-start justify-center">
           <div className="outer-box space-y-[14px]">
             {/* This is the "client island" - only this part becomes client-side */}
             <AppClient />
