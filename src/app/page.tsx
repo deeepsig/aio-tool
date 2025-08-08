bf541f501cb13f3c14046e27171bd79e30fcf37a
deeepsig <deepakgdkg1g8868@gmail.com>
2025-08-08 14:51:45 +0530

    Added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import AppClient from '@/components/app-client';
 import About from '@/components/misc/about';
+import Footer from '@/components/misc/footer';
 
 export default function Page() {
   return (
-    <div className="min-h-screen font-sans">
-      <div className="flex flex-col items-center py-[8vh]">
+    <div className="min-h-screen font-sans flex flex-col">
+      <div className="flex-1 flex flex-col items-center py-[8vh]">
         <About />
         <div className="mt-8 flex items-start justify-center">
           <div className="outer-box space-y-[14px]">
@@ -13,6 +14,7 @@ export default function Page() {
           </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
