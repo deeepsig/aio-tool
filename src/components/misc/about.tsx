91a805d9d77fa3e428b0ebe2ad78bc9552ed2ca5
deeepsig <deepakgdkg1g8868@gmail.com>
2025-08-08 16:27:50 +0530

    responsive about

--- src/components/misc/about.tsx
+++ src/components/misc/about.tsx
@@ -1,10 +1,10 @@
 export default function About() {
   return (
     <div className="text-center mb-6 space-y-1">
-      <h1 className="font-vt323 text-5xl text-[#D9D9D9]">
+      <h1 className="font-vt323 text-4xl sm:text-5xl text-[#D9D9D9]">
         Optimize {<span className="text-[#696969]">robots.txt</span>}
       </h1>
-      <h2 className="font-sans font-light text-xl text-[#D9D9D9]">
+      <h2 className="font-sans font-light text-lg sm:text-xl text-[#D9D9D9]">
         Is your site still relevant??
       </h2>
     </div>
